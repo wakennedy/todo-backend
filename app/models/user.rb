2bfd3f2819cfcd0aa56ef0b728829a6f00f3a3b2
wakennedy <alexkenned@gmail.com>
2020-10-05 11:03:21 -0700

    init: db, relations, schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :user_items
+    has_many :items, through: :user_items
+
+    validates :username, uniqueness: true
+
 end
--- db/migrate/20201005174602_create_user_items.rb
+++ db/migrate/20201005174602_create_user_items.rb
@@ -1,6 +1,8 @@
 class CreateUserItems < ActiveRecord::Migration[6.0]
   def change
     create_table :user_items do |t|
+      t.integer :user_id
+      t.integer :item_id
 
       t.timestamps
     end
